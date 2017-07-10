ef76e2b043b53dae4a02cbfd0fbf5ede8adbc6c5
Rakshak <rakshakmanandhar4@gmail.com>
2017-07-10 19:07:31 +0545

    React Native Camera

---
+++ app/src/main/java/com/easysolution/camerapreview/MainActivity.java
@@ -0,0 +1,46 @@
+package com.easysolution.camerapreview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ReactCamera.RNCameraViewPackage;
+import com.facebook.react.LifecycleState;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
+
+public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
+
+
+    private ReactRootView mReactRootView;
+    private ReactInstanceManager mReactInstanceManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mReactRootView = new ReactRootView(this);
+
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModuleName("index.android")
+                .addPackage(new MainReactPackage())
+
+                .addPackage(new RNCameraViewPackage())
+
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+
+        mReactRootView.startReactApplication(mReactInstanceManager, "ExampleRN", null);
+
+        setContentView(mReactRootView);
+    }
+
+
+    @Override
+    public void invokeDefaultOnBackPressed() {
+
+    }
+}
